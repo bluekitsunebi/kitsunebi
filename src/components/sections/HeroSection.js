657b84f7b9eae6ec9fc53ee1c4c18fab95ee6119
Cezara24 <cezarateodorescu8@gmail.com>
2023-03-30 20:21:16 +0300

    completed the animation for the hero section

--- src/components/sections/HeroSection.js
+++ src/components/sections/HeroSection.js
@@ -6,11 +6,24 @@ function HeroSection() {
     <section id="heroSection" className={styles.heroSection}>
       <div className={styles.cover}>
         
-        <div className={`${styles.halfBackground} ${styles.halfBackground__left}`}>
-          <div className={styles.coverContentContainer}>
+        <div 
+          id="halfBackgroundLeft" 
+          className={`${styles.halfBackground} ${styles.halfBackground__left}`}
+          onMouseOver={() => {
+            document.getElementById("titleLeft").innerHTML = "Language Courses"
+            document.getElementById("titleRight").innerHTML = "Programming<br>Courses"
+            document.getElementById("halfBackgroundLeft").style.width = (window.innerWidth / 100 * 70).toString() + "px"
+            document.getElementById("halfBackgroundRight").style.width = (window.innerWidth / 100 * 50).toString() + "px"
+            document.getElementById("halfBackgroundLeft").style.clipPath = "polygon(0% 0%, 0% 100%, 71.42% 100%, 100% 0%)"
+            document.getElementById("halfBackgroundRight").style.clipPath = "polygon(40% 0%, 0% 100%, 100% 100%, 100% 0%)"
+            document.getElementById("coverContentContainerLeft").style.width = "50vw"
+            document.getElementById("coverContentContainerRight").style.width = "30vw"
+          }}
+        >
+          <div id="coverContentContainerLeft" className={styles.coverContentContainer}>
             <div className={styles.topCover}></div>
             <div className={styles.coverText}>
-              <h1 className={styles.title}>Language Courses</h1>
+              <h1 id="titleLeft" className={styles.title}>Language Courses</h1>
               <p className={styles.subtitle}>
                 Japanese
               </p>
@@ -21,15 +34,39 @@ function HeroSection() {
                 Romanian
               </p>
             </div>
-
-
           </div>
         </div>
 
-        <div className={`${styles.halfBackground} ${styles.halfBackground__right}`}>
-
+        <div 
+          id="halfBackgroundRight"
+          className={`${styles.halfBackground} ${styles.halfBackground__right}`}
+          onMouseOver={() => {
+            document.getElementById("titleLeft").innerHTML = "Language<br>Courses"
+            document.getElementById("titleRight").innerHTML = "Programming Courses"
+            document.getElementById("halfBackgroundLeft").style.width = (window.innerWidth / 100 * 50).toString() + "px"
+            document.getElementById("halfBackgroundRight").style.width = (window.innerWidth / 100 * 70).toString() + "px"
+            document.getElementById("halfBackgroundLeft").style.clipPath = "polygon(0% 0%, 0% 100%, 60% 100%, 100% 0%)"
+            document.getElementById("halfBackgroundRight").style.clipPath = "polygon(28.57% 0%, 0% 100%, 100% 100%, 100% 0%)"
+            document.getElementById("coverContentContainerLeft").style.width = "30vw"
+            document.getElementById("coverContentContainerRight").style.width = "50vw"
+          }}
+        >
+          <div id="coverContentContainerRight" className={`${styles.coverContentContainer} ${styles.coverContentContainer__right}`}>
+            <div className={styles.topCover}></div>
+            <div className={styles.coverText}>
+              <h1 id="titleRight" className={styles.title}>Programming Courses</h1>
+              <p className={styles.subtitle}>
+                HTML
+              </p>
+              <p className={styles.subtitle}>
+                CSS
+              </p>
+              <p className={styles.subtitle}>
+                JavaScript
+              </p>
+            </div>
+          </div>
         </div>
-
       </div>
     </section>
   );
